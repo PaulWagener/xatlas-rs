4f908088dda316e614623036540bf60426f5cea7
Paul Wagener <mail@paulwagener.nl>
2023-12-15 22:12:10 +0100

    Support static_crt build via environment variable

--- build.rs
+++ build.rs
@@ -2,12 +2,22 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    cc::Build::new()
+    let mut build = cc::Build::new();
+    build
         .file("vendor/source/xatlas/xatlas.cpp")
         .flag("-std=c++11")
         .cpp(true)
-        .warnings(false)
-        .compile("xatlas");
+        .warnings(false);
+
+    if let Ok(crt) = env::var("XATLAS_MSVC_CRT") {
+        match crt.as_str() {
+            "dynamic" => build.static_crt(false),
+            "static" => build.static_crt(true),
+            _ => panic!("Invalid value of OPENCV_MSVC_CRT var, expected \"static\" or \"dynamic\""),
+        };
+    }
+
+    build.compile("xatlas");
 
     bindgen::builder()
         .header("vendor/source/xatlas/xatlas.h")
